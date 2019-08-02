772c4ed034b3c815d997fb7636c1a1c696a095e7
Mickaël <mickael@MacBook-Air-de-Mickael.local>
2019-08-02 21:04:57 +0200

    Add app/fondateurs and assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { FondateursComponent } from './fondateurs/fondateurs.component';
 
 const routes: Routes = [
 
   { path: '', redirectTo:'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
+  { path: 'fondateurs', component: FondateursComponent},
 
   ];
 
